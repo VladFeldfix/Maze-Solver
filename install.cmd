33ddaafad9555e6fb875dbfc7696ae8de793b5fe
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-10-29 00:26:59 +0200

    general sync

--- install.cmd
+++ install.cmd
@@ -1,1 +1,3 @@
-pyinstaller -i favicon.ico --onefile --noconsole Maze-Solver.py
+set "currentDirectory=%cd%
+pyinstaller --distpath %currentDirectory% -i favicon.ico --onefile --noconsole Maze-Solver.py
+pause
